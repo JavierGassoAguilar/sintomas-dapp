d4b4f63a309201f63b8c9f608d5efe76f3253104
Javier Gasso <gassoaguilar@msn.com>
2019-12-05 20:10:59 +0100

    Estrucutra del sintoma

--- contracts/SintomasDapp.sol
+++ contracts/SintomasDapp.sol
@@ -1,7 +1,36 @@
 pragma solidity ^0.5.0;
 
 contract SintomasDapp {
-  uint storedData;
+
+  /*
+    sintomas
+    1. Texto del sintoma
+   
+    remedios
+    2. Direccion del usuario que ha registrado el remedio
+    3. Texto del remedio
+
+    puntuaciones del remedio
+    4. Acumulador de calificacion
+    5. Numero de votos
+
+  */
+
+  struct Sintoma {
+    uint id;
+    string sintoma;
+    address remediador;
+    string remedio;
+    uint sumaCalificacion;
+    uint numeroVotos;
+  }
+  
+  // uint ultimoId;
+  // mapping para acceder a los campos de los sintomas
+
+  // que funciones hacen falta en el contrato (nombres y parametros)
+
+
 
   function set(uint x) public {
     storedData = x;
