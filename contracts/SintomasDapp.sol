260adba9a58a622971020cc9f4afd4334c7b820a
Javier Gasso <gassoaguilar@msn.com>
2019-12-10 20:02:35 +0100

    avances en el diseño del contrato

--- contracts/SintomasDapp.sol
+++ contracts/SintomasDapp.sol
@@ -20,16 +20,43 @@ contract SintomasDapp {
     uint id;
     string sintoma;
     address remediador;
+    string tipoRemedio;
     string remedio;
-    uint sumaCalificacion;
-    uint numeroVotos;
+    uint sumaCalificacionRemedio;
+    uint numeroVotosRemedio;
+  }
+
+  struct Remediador {
+    address remediador;
+    uint sumaCalificacionGlobal;
+    uint numeroVotosGlobal;
   }
   
-  // uint ultimoId;
-  // mapping para acceder a los campos de los sintomas
+  mapping (tipoVariable => nombreEstructura) nombreVariable;
+  // Crear mapping para los sintomas
+  // Crear mapping para el remediador
+
+  // Crear variable de esto para guardar el id del ultimo sintoma
+  uint ultimoId;
+
 
   // que funciones hacen falta en el contrato (nombres y parametros)
+  // function registro (msg.sender) -> No es necesario
+  function tengoUnSintoma (string dolorConccreto)
+  //function buscoRemedio (string dolorConccreto) -> Lo vamos a hacer en react
+  function tengoRemedio (uint id, string remedio string tipoRemedio)
+  function valoroRemedio (uint id, uint8 valoracion(1-10))
+  // function equipararNotaRemedioYnotaMsg.sender -> A la hora de valorarRemedio sumaremos la nota en las dos estructuras
 
+  /*
+    tipos de remedio: (Vamos a hacer el selector/menu en React)
+    -posturas de yoga.
+    -infusiones.
+    -homeopatia.
+    -otros.
+  */
+
+  // Hacer las 3 funciones de arriba
 
 
   function set(uint x) public {
